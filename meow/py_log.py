9494985990c1e6bfae11a5a1e95629f8aced9610
credwwood <136933083+credwwood@users.noreply.github.com>
2024-02-07 20:34:29 +0300

    Add files via upload

---
+++ meow/py_log.py
@@ -0,0 +1,21 @@
+INFO:root:An INFO
+WARNING:root:A WARNING
+ERROR:root:AN ERROR
+CRITICAL:root:A message of CRITICAL severity
+INFO:root:The values of vars is x=3 and y=4
+INFO:root:x/y successful with result: 0.75
+INFO:root:The values of vars is x=2 and y=5
+INFO:root:x/y successful with result: 0.4
+INFO:root:The values of vars is x=3 and y=7
+INFO:root:x/y successful with result: 0.42857142857142855
+INFO:root:The values of vars is x=6 and y=12
+INFO:root:x/y successful with result: 0.5
+INFO:root:The values of vars is x=4 and y=0
+ERROR:root:ZeroDivisionError
+Traceback (most recent call last):
+  File "/tmp/ipykernel_15842/2522966082.py", line 8, in <module>
+    x/y
+    ~^~
+ZeroDivisionError: division by zero
+INFO:root:The values of vars is x=10 and y=1
+INFO:root:x/y successful with result: 10.0
